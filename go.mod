70055f9ddb00043950d0c04c499bd521966c6792
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-06-05 14:58:06 +0200

    form: implement base form's structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gucio321/forms-go
 
 go 1.18
+
+require github.com/gocarina/gocsv v0.0.0-20220531201732-5f969b02b902 // indirect
--- pkg/forms/forms.go
+++ pkg/forms/forms.go
@@ -1,9 +1,59 @@
 package forms
 
+import (
+	"fmt"
+
+	"github.com/gocarina/gocsv"
+)
+
+// QuestionType represents a type of question
 type QuestionType byte
 
-type Form struct{}
+// types of question in a form
+const (
+	QuestionTypeSeparator QuestionType = iota
+	QuestionTypeText
+	QuestionTypeTextArea
+	QuestionTypeCheckbox
+	QuestionTypeRadio
+	QuestionTypeSelect
+)
 
-func NewForm() {
-	return Form{}
+// Question represents a question in a form
+type Question struct {
+	Text        string       `csv:"text"`
+	Type        QuestionType `csv:"type"`
+	Placeholder string       `csv:"placeholder"`
+	Options     []string     `csv:"options"`
+	Answer      string       `csv:"answer"`
+}
+
+// Form represents a parsable form.
+type Form struct {
+	Questions []*Question
+}
+
+// NewForm returns a new form
+func NewForm() *Form {
+	return &Form{
+		Questions: make([]*Question, 0),
+	}
+}
+
+// Parse parses a form from a byte array
+func (f *Form) Parse(data []byte) error {
+	if err := gocsv.UnmarshalBytes(data, &f.Questions); err != nil {
+		return fmt.Errorf("error parsing data: %w", err)
+	}
+
+	return nil
+}
+
+// Marshal marshals a form to a byte array
+func (f *Form) Marshal() (data []byte, err error) {
+	if data, err = gocsv.MarshalBytes(f.Questions); err != nil {
+		return nil, fmt.Errorf("error marshalling data: %w", err)
+	}
+
+	return data, nil
 }
