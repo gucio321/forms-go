d736444ed23a0b5b081d53401dea514c8d0912ab
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-06-05 14:40:38 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gucio321/forms-go
+
+go 1.18
---
+++ pkg/forms/doc.go
@@ -0,0 +1,10 @@
+// Package forms is used to parse and create csv output for forms
+package forms
+
+type QuestionType byte
+
+type Form struct{}
+
+func NewForm() {
+	return Form{}
+}
